16b067f57b349b4006ceb94b419129571f1bd4c9
Osuchov <osuchto@wp.pl>
2017-04-15 19:03:18 +0200

    Zadanie 1

--- zadanie1/index.php
+++ zadanie1/index.php
@@ -1,1 +1,19 @@
 <?php
+if ($_SERVER['REQUEST_METHOD']==='POST') {
+    if (is_string($_POST['name']) && strlen($_POST['name'])>0) {
+        echo 'Cześć, '. $_POST['name'] .'<br>';
+    }
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+    <form action="" method="post" role="form">
+                    <label for="Username">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Username...">
+                <button type="submit">Send!</button>
+    </form>
+</body>
+</html>
